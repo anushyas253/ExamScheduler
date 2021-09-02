2bf144c790712179d0ef536f99fbc7fe9ff462e9
anushyas253 <anushya253@gmail.com>
2021-09-02 13:50:27 +0530

    Added code to get indv and org names for firebase

--- app/src/main/java/com/example/internalproject/view/HomeFragment.kt
+++ app/src/main/java/com/example/internalproject/view/HomeFragment.kt
@@ -2,9 +2,19 @@ package com.example.internalproject.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.example.internalproject.presenter.FirebasePresenter
+import com.example.internalproject.presenter.IndvProfilePresenter
 
 class HomeFragment: Fragment() {
+    lateinit var reference: FirebasePresenter
+    lateinit var profilePresenter : IndvProfilePresenter
+    lateinit var currentUserId : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var IndvName=profilePresenter.nameE
+        var OrgName=profilePresenter.userProfileName
     }
+
 }
